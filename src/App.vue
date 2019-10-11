08f249b11fa2805dac2146f33a81deb8b8eb5d90
jiangxuhang <672500201@qq.com>
2019-10-11 21:19:30 +0800

    vuex font scss icon

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,36 @@
 <template>
   <div id="app">
+    <span class="text">ABCDEFG</span>
     <router-view/>
   </div>
 </template>
+
+<script>
+import {mapGetters} from 'vuex'
+export default {
+  computed: {
+    ...mapGetters(['test'])
+  },
+  mounted() {
+    this.$store.dispatch('setTest', 9).then(() => {
+      console.log(this.test)
+    })
+  }
+}
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html')
+  let fontSize = window.innerWidth / 10
+  if(fontSize > 50) {
+    fontSize = 50
+  }
+  html.style.fontSize = fontSize + 'px'
+})
+</script>
+
+<style lang="scss">
+@import "./assets/styles/global";
+  .text {
+    font-family: "Days One";
+    font-size: px2rem(37.5);
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <div id="read"></div>
+    <span class="icon-bookmark"></span>
   </div>
 </template>
 
@@ -8,13 +8,5 @@
 import Epub from 'epubjs'
 global.epub = Epub
 export default {
-  mounted () {
-    this.book = new Epub('/漆黑的子弹1：以神为目标的人们.epub')
-    this.book.renderTo('read', {
-      width: window.innerWidth,
-      height: window.innerHeight
-    }).display()
-    console.log(this.book)
-  }
 }
 </script>
