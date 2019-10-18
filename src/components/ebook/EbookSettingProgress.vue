618c65c992270c5af6ba9fdcab45e2d337572def
jiangxuhang <20157008@s.hlju.edu.cn>
2019-10-18 22:20:30 +0800

    debug

--- src/components/ebook/EbookSettingProgress.vue
+++ src/components/ebook/EbookSettingProgress.vue
@@ -39,12 +39,13 @@ import { getReadTimeByMinute } from '../../utils/book'
     mixins: [ebookMixin],
     computed: {
       getSectionName() {
-        if(this.section) {
-          const sectionInfo = this.currentBook.section(this.section)
-          if(sectionInfo && sectionInfo.href && this.currentBook && this.currentBook.navigation) {
-            return this.currentBook.navigation.get(sectionInfo.href).label
-          }
-        }
+        // if(this.section) {
+        //   const sectionInfo = this.currentBook.section(this.section)
+        //   if(sectionInfo && sectionInfo.href && this.currentBook && this.currentBook.navigation) {
+        //     return this.currentBook.navigation.get(sectionInfo.href).label
+        //   }
+        // }
+        return this.section ? this.navigation[this.section].label : ''
       }
     },
     methods: {
