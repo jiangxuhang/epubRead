c400286b2590b2cc0db5c7d1062a158f31ebccb2
jiangxuhang <20157008@s.hlju.edu.cn>
2019-10-25 22:59:53 +0800

    flapCard

--- src/components/home/FlapCard.vue
+++ src/components/home/FlapCard.vue
@@ -1,5 +1,20 @@
 <template>
     <div class="flap-card-wrapper" v-show="flapCardVisible">
+        <div class="flap-card-bg">
+            <div class="flap-card" v-for="(item, index) in flapCardList" :key="index"
+            :style="{zIndex: item.zIndex}">
+                <div class="flap-card-circle">
+                    <div class="flap-card-semi-circle flap-card-semi-circle-left"
+                    :style="semiCircleStyle(item, 'left')" ref="left">
+
+                    </div>
+                    <div class="flap-card-semi-circle flap-card-semi-circle-right"
+                    :style="semiCircleStyle(item, 'right')" ref="right">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="close-btn-wrapper" @click="close">
             <span class="icon-close"></span>
         </div>
@@ -8,12 +23,92 @@
 
 <script>
 import {storeHomeMixin} from '../../utils/mixin'
+import { flapCardList } from '../../utils/store'
 export default {
     mixins: [storeHomeMixin],
+    data() {
+        return {
+            flapCardList,
+            front: 0,
+            back: 1
+        }
+    },
     methods: {
         close() {
             this.setFlapCardVisible(false)
+        },
+        semiCircleStyle(item, dir) {
+            return  {
+                backgroundColor: `rgb(${item.r}, ${item.g}, ${item.b})`,
+                backgroundSize: item.backgroundSize,
+                backgroundImage: dir === 'left' ? item.imgLeft : item.imgRight
+            }
+        },
+        rotate(index, type) {
+            const item = this.flapCardList[index]
+            let dom
+            if (type === 'front') {
+            dom = this.$refs.right[index]
+            } else {
+            dom = this.$refs.left[index]
+            }
+            dom.style.transform = `rotateY(${item.rotateDegree}deg)`
+            dom.style.backgroundColor = `rgb(${item.r}, ${item._g}, ${item.b})`
+        },
+        flapCardRotate() {
+            const frontFlapCard = this.flapCardList[this.front]
+            const backFlapCard = this.flapCardList[this.back]
+            frontFlapCard.rotateDegree += 10
+            frontFlapCard._g -= 5
+            backFlapCard.rotateDegree -= 10
+            backFlapCard._g += 5
+            if(frontFlapCard.rotateDegree === 90 && backFlapCard.rotateDegree === 90) {
+                backFlapCard.zIndex += 2
+            }
+            this.rotate(this.front, 'front')
+            this.rotate(this.back, 'back')
+            if(frontFlapCard.rotateDegree === 180 && backFlapCard.rotateDegree === 0) {
+                this.next()
+            }
+        },
+        next() {
+            const frontFlapCard = this.flapCardList[this.front]
+            const backFlapCard = this.flapCardList[this.back]
+            frontFlapCard.rotateDegree = 0
+            backFlapCard.rotateDegree = 0
+            frontFlapCard._g = frontFlapCard.g
+            backFlapCard._g = backFlapCard.g
+            this.rotate(this.front, 'front')
+            this.rotate(this.back, 'back')
+            this.front++
+            this.back++
+            const len = this.flapCardList.length
+            if(this.front >= len) {
+                this.front = 0
+            }
+            if(this.back >= len) {
+                this.back = 0
+            }
+            this.flapCardList.forEach((item, index) => {
+                item.zIndex = 100 - ((index - this.front + len) % len)
+            })
+            this.prepare()
+        },
+        prepare() {
+            const backFlapCard = this.flapCardList[this.back]
+            backFlapCard.rotateDegree = 180
+            backFlapCard._g = backFlapCard.g - 5 * 18
+            this.rotate(this.back, 'back')
+        },
+        startFlapCardAnimation() {
+            this.prepare()
+            setInterval(() => {
+                this.flapCardRotate()
+            }, 25)
         }
+    },
+    mounted() {
+        this.startFlapCardAnimation()
     }
 }
 </script>
@@ -21,15 +116,46 @@ export default {
 <style lang="scss">
 @import "../../assets/styles/global";
 .flap-card-wrapper {
-    position: absolute;
-    top:0;
-    left:0;
-    right:0;
-    bottom:0;
     z-index: 1000;
     height: 100%;
     width: 100%;
     background: rgba(0,0,0,.6);
+    @include center;
+    @include absCenter;
+    .flap-card-bg {
+        position: relative;
+        width: px2rem(64);
+        height: px2rem(64);
+        border-radius: px2rem(5);
+        background: white;
+        .flap-card {
+            width: px2rem(48);
+            height: px2rem(48);
+            @include absCenter;
+            .flap-card-circle {
+                width: 100%;
+                height: 100%;
+                display:flex;
+                .flap-card-semi-circle {
+                    flex: 0 0 50%;
+                    width: 50%;
+                    height: 100%;
+                    background-repeat: no-repeat;
+                    backface-visibility: hidden;
+                }
+                .flap-card-semi-circle-left {
+                    border-radius: px2rem(24) 0 0 px2rem(24);
+                    background-position: center right;
+                    transform-origin: right;
+                }
+                .flap-card-semi-circle-right {
+                    border-radius: 0 px2rem(24) px2rem(24) 0;
+                    background-position: center left;
+                    transform-origin: left;
+                }
+            }
+        }
+    }
     .close-btn-wrapper {
         position: absolute;
         left: 0;
@@ -38,7 +164,13 @@ export default {
         width: 100%;
         @include center;
         .icon-close {
-
+            width: px2rem(45);
+            height: px2rem(45);
+            font-size: px2rem(25);
+            border-radius: 50%;
+            background: #333;
+            color: white;
+            @include center;
         }
     }
 }
