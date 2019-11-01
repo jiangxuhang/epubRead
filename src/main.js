5b69c098e9eae525972b1f3c945ce42715c84efa
jiangxuhang <20157008@s.hlju.edu.cn>
2019-11-01 22:18:03 +0800

    new component

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 import i18n from './lang'
 import './assets/styles/icon.css'
 import './assets/styles/global.scss'
+import './utils/boost'
+import './utils/create-api'
 
 Vue.config.productionTip = false
 
